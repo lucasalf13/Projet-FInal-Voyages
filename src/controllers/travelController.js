388ebe23c887f94053abe093569fa7f2c87862b0
lucasalf13 <lucas.alf@outlook.com>
2025-07-24 12:09:41 +0200

    a

--- src/controllers/travelController.js
+++ src/controllers/travelController.js
@@ -211,7 +211,10 @@ exports.downloadPDF = async (req, res) => {
             return res.status(500).send("Erreur lors du rendu PDF");
         }
         try {
-            const browser = await puppeteer.launch();
+const browser = await puppeteer.launch({
+    headless: true,
+    args: ['--no-sandbox', '--disable-setuid-sandbox']
+});
             const page = await browser.newPage();
             await page.setContent(html, { waitUntil: 'networkidle0' });
             const pdfBuffer = await page.pdf({ format: 'A4', printBackground: true });
